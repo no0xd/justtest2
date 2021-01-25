496f1dc5501d3ad9cc2b8e9e11ea739da19cb791
hjx <123@qq.com>
2021-01-25 15:09:57 +0800

    d delete all

--- 1400-client8/src/test/java/com/netposa/gat/TestStr.java
+++
@@ -1,7 +0,0 @@
-package com.netposa.gat;
-
-public class TestStr {
-	public static void main(String[] args) {
-		System.out.println("000000000000000000000000008444991504939520601795".length());
-	}
-}
--- 1400-client8/version/gat1400-client/start_client.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-title %~f0
-
-java.exe -jar gat1400-client-1.0.0.jar
-pause
-
